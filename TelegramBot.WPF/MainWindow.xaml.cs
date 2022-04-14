6c61ccb5952e68ae2d6700004fa61ce00a22cebe
Ilya Bozhkov <ilia.il.27@mail.ru>
2022-04-15 00:59:27 +0300

    Add WPF logic

--- TelegramBot.WPF/MainWindow.xaml.cs
+++ TelegramBot.WPF/MainWindow.xaml.cs
@@ -25,96 +25,176 @@ namespace TelegramBot.WPF
             InitializeComponent();
         }
 
-        private void Button_SendQuestion_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void ComboBox_ChooseQuestionType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (ComboBox_ChooseQuestionType.SelectedIndex != -1)
+            {
+                RadioButton_Test.Visibility = Visibility.Visible;
+                RadioButton_Test.IsChecked = false;
+                RadioButton_Poll.Visibility = Visibility.Visible;
+                RadioButton_Poll.IsChecked = false;
+                TextBox_Question.Visibility = Visibility.Visible;
+                Lable_QuestionTitle.Visibility = Visibility.Visible;
+            }
+
             switch (ComboBox_ChooseQuestionType.SelectedIndex)
             {
                 case 0:
-                    //TextBox_Question.Visibility = Visibility.Visible;
-                    TextBox_Variants.Visibility = Visibility.Hidden;
-                    Button_AddVariant.Visibility = Visibility.Hidden;
-                    Button_RemoveVariant.Visibility = Visibility.Hidden;
-                    RadioButton_Test.Visibility = Visibility.Visible;
-                    RadioButton_Poll.Visibility = Visibility.Visible;
+                    TextBox_VariantsChoseOneOfFew.Visibility = Visibility.Hidden;
+                    Button_AddVariantChoseOneOfFew.Visibility = Visibility.Hidden;
+                    Button_RemoveVariantChoseOneOfFew.Visibility = Visibility.Hidden;
+
+                    TextBox_VariantsChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_AddVariantChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_RemoveVariantChosenMultiple.Visibility = Visibility.Hidden;
+
+                    Label_TrueAnswer.Visibility = Visibility.Hidden;
+                    TextBox_TrueAnswerChoseOneOfFew.Visibility = Visibility.Hidden;
+
+                    TextBox_TrueAnswerChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_AddAnswer.Visibility = Visibility.Hidden;
+                    Button_RemoveAnswer.Visibility = Visibility.Hidden;
+                    ChoseMultiple.Visibility = Visibility.Hidden;
+
                     RadioButton_Yes.Visibility = Visibility.Hidden;
                     RadioButton_No.Visibility = Visibility.Hidden;
+                    YesOrNo.Visibility = Visibility.Hidden;
+
                     break;
                 case 1:
-                    //TextBox_Question.Visibility = Visibility.Visible;
-                    TextBox_Variants.Visibility = Visibility.Hidden;
-                    Button_AddVariant.Visibility = Visibility.Hidden;
-                    Button_RemoveVariant.Visibility = Visibility.Hidden;
-                    RadioButton_Test.Visibility = Visibility.Visible;
-                    RadioButton_Poll.Visibility = Visibility.Visible;
+                    TextBox_VariantsChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_AddVariantChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_RemoveVariantChosenMultiple.Visibility = Visibility.Hidden;
+
+                    Label_TrueAnswer.Visibility = Visibility.Hidden;
+                    TextBox_AnswerUserEnter.Visibility = Visibility.Hidden;
+
+                    TextBox_TrueAnswerChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_AddAnswer.Visibility = Visibility.Hidden;
+                    Button_RemoveAnswer.Visibility = Visibility.Hidden;
+                    ChoseMultiple.Visibility = Visibility.Hidden;
+
                     RadioButton_Yes.Visibility = Visibility.Hidden;
                     RadioButton_No.Visibility = Visibility.Hidden;
+                    YesOrNo.Visibility = Visibility.Hidden;
                     break;
                 case 2:
-                    //TextBox_Question.Visibility = Visibility.Visible;
-                    TextBox_Variants.Visibility = Visibility.Visible;
-                    Button_AddVariant.Visibility = Visibility.Visible;
-                    Button_RemoveVariant.Visibility = Visibility.Visible;
-                    RadioButton_Test.Visibility = Visibility.Visible;
-                    RadioButton_Poll.Visibility = Visibility.Visible;
+                    TextBox_VariantsChoseOneOfFew.Visibility = Visibility.Hidden;
+                    Button_AddVariantChoseOneOfFew.Visibility = Visibility.Hidden;
+                    Button_RemoveVariantChoseOneOfFew.Visibility = Visibility.Hidden;
+
+                    Label_TrueAnswer.Visibility = Visibility.Hidden;
+                    TextBox_AnswerUserEnter.Visibility = Visibility.Hidden;
+
+                    Label_TrueAnswer.Visibility = Visibility.Hidden;
+                    TextBox_TrueAnswerChoseOneOfFew.Visibility = Visibility.Hidden;
+
                     RadioButton_Yes.Visibility = Visibility.Hidden;
                     RadioButton_No.Visibility = Visibility.Hidden;
+                    YesOrNo.Visibility = Visibility.Hidden;
                     break;
                 case 3:
-                    //TextBox_Question.Visibility = Visibility.Visible;
-                    RadioButton_Yes.Visibility = Visibility.Visible;
-                    RadioButton_No.Visibility = Visibility.Visible;
-                    TextBox_Variants.Visibility = Visibility.Hidden;
-                    Button_AddVariant.Visibility = Visibility.Hidden;
-                    Button_RemoveVariant.Visibility = Visibility.Hidden;
+                    TextBox_VariantsChoseOneOfFew.Visibility = Visibility.Hidden;
+                    Button_AddVariantChoseOneOfFew.Visibility = Visibility.Hidden;
+                    Button_RemoveVariantChoseOneOfFew.Visibility = Visibility.Hidden;
+
+                    TextBox_VariantsChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_AddVariantChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_RemoveVariantChosenMultiple.Visibility = Visibility.Hidden;
+
+                    Label_TrueAnswer.Visibility = Visibility.Hidden;
+                    TextBox_AnswerUserEnter.Visibility = Visibility.Hidden;
+
+                    Label_TrueAnswer.Visibility = Visibility.Hidden;
+                    TextBox_TrueAnswerChoseOneOfFew.Visibility = Visibility.Hidden;
+
+                    TextBox_TrueAnswerChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_AddAnswer.Visibility = Visibility.Hidden;
+                    Button_RemoveAnswer.Visibility = Visibility.Hidden;
+                    ChoseMultiple.Visibility = Visibility.Hidden;
+
                     break;
                 case 4:
-                    RadioButton_Test.Visibility = Visibility.Visible;
-                    RadioButton_Poll.Visibility = Visibility.Visible;
-                    TextBox_Variants.Visibility = Visibility.Visible;
-                    Button_AddVariant.Visibility = Visibility.Visible;
-                    Button_RemoveVariant.Visibility = Visibility.Visible;
-                    RadioButton_Yes.Visibility = Visibility.Hidden;
-                    RadioButton_No.Visibility = Visibility.Hidden;
+
                     break;
-                //case 5:
-                //    TextBox_Question.Visibility = Visibility.Hidden;
-                //    TextBox_Variants.Visibility = Visibility.Hidden;
-                //    Button_AddVariant.Visibility = Visibility.Hidden;
-                //    Button_RemoveVariant.Visibility = Visibility.Hidden;
-                //    RadioButton_Test.Visibility = Visibility.Visible;
-                //    RadioButton_Poll.Visibility = Visibility.Visible;
-                //    break;
-                default:
-                    RadioButton_Test.Visibility = Visibility.Hidden;
-                    RadioButton_Poll.Visibility = Visibility.Hidden;
+            }
+
+            switch (ComboBox_ChooseQuestionType.SelectedIndex)
+            {
+
+                case 1:
+                    TextBox_VariantsChoseOneOfFew.Visibility = Visibility.Visible;
+                    Button_AddVariantChoseOneOfFew.Visibility = Visibility.Visible;
+                    Button_RemoveVariantChoseOneOfFew.Visibility = Visibility.Visible;
+                    break;
+                case 2:
+                    TextBox_VariantsChosenMultiple.Visibility = Visibility.Visible;
+                    Button_AddVariantChosenMultiple.Visibility = Visibility.Visible;
+                    Button_RemoveVariantChosenMultiple.Visibility = Visibility.Visible;
+                    break;
+ 
+                case 4:
+
                     break;
             }
         }
 
         private void RadioButton_Test_Click(object sender, RoutedEventArgs e)
         {
-            TextBox_TrueAnswer.Visibility = Visibility.Visible;
-            Button_AddAnswer.Visibility = Visibility.Visible;
-            Button_RemoveAnswer.Visibility = Visibility.Visible;
-            Label_TrueAnswer.Visibility = Visibility.Visible;
+            switch (ComboBox_ChooseQuestionType.SelectedIndex)
+            {
+                case 0:
+                    Label_TrueAnswer.Visibility = Visibility.Visible;
+                    TextBox_AnswerUserEnter.Visibility = Visibility.Visible;
+                    break;
+                case 1:
+                    Label_TrueAnswer.Visibility = Visibility.Visible;
+                    TextBox_TrueAnswerChoseOneOfFew.Visibility = Visibility.Visible;
+                    break;
+                case 2:
+                    TextBox_TrueAnswerChosenMultiple.Visibility = Visibility.Visible;
+                    Button_AddAnswer.Visibility = Visibility.Visible;
+                    Button_RemoveAnswer.Visibility = Visibility.Visible;
+                    ChoseMultiple.Visibility = Visibility.Visible;
+                    break;
+                case 3:
+                    RadioButton_Yes.Visibility = Visibility.Visible;
+                    RadioButton_No.Visibility = Visibility.Visible;
+                    YesOrNo.Visibility = Visibility.Visible;
+                    break;
+                case 4:
+
+                    break;
+            }
         }
 
         private void RadioButton_Poll_Click(object sender, RoutedEventArgs e)
         {
-            TextBox_TrueAnswer.Visibility = Visibility.Hidden;
-            Button_AddAnswer.Visibility = Visibility.Hidden;
-            Button_RemoveAnswer.Visibility = Visibility.Hidden;
-            Label_TrueAnswer.Visibility = Visibility.Hidden;
+            switch (ComboBox_ChooseQuestionType.SelectedIndex)
+            {
+                case 0:
+                    Label_TrueAnswer.Visibility = Visibility.Hidden;
+                    TextBox_AnswerUserEnter.Visibility = Visibility.Hidden;
+                    break;
+                case 1:
+                    Label_TrueAnswer.Visibility = Visibility.Hidden;
+                    TextBox_TrueAnswerChoseOneOfFew.Visibility = Visibility.Hidden;
+                    break;
+                case 2:
+                    TextBox_TrueAnswerChosenMultiple.Visibility = Visibility.Hidden;
+                    Button_AddAnswer.Visibility = Visibility.Hidden;
+                    Button_RemoveAnswer.Visibility = Visibility.Hidden;
+                    ChoseMultiple.Visibility = Visibility.Hidden;
+                    break;
+                case 3:
+                    RadioButton_Yes.Visibility = Visibility.Hidden;
+                    RadioButton_No.Visibility = Visibility.Hidden;
+                    YesOrNo.Visibility = Visibility.Hidden;
+                    break;
+                case 4:
+                    break;
+            }
         }
 
-        private void Button_RemoveAnswer_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
