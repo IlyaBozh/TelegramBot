78a37f79c494a8b4e8ffdcdfff8e1d9dbe30c522
Lev <leva.100s@gmail.com>
2022-04-15 13:57:23 +0300

    Add Class ChosenYesOrNo

---
+++ TelegramBot.BLL/ChosenYesOrNo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.BLL
+{ 
+    public class ChosenYesOrNo : IQuestion
+    {
+
+        public string TrueAnswer { get; set; }
+        public string AllAnswer { get; set; }
+
+        public ChosenYesOrNo ( string trueAnswer, string allAnswer )
+        {
+            TrueAnswer = trueAnswer;
+            AllAnswer = allAnswer;
+        }
+
+        public void EditQuestion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
