4b156aca2ac86d16ebb217f8bfc5d8b43c9b7a82
Lev <leva.100s@gmail.com>
2022-04-15 21:47:09 +0300

    Add Class Arrange Correct

---
+++ TelegramBot.BLL/ArrangeCorrect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.BLL
+{
+    internal class ArrangeCorrect : IQuestion
+    {
+
+        public  string Question { get; set; }
+
+        public List <string> AllAnswers { get; set; }
+        public List <string> TrueAnswer { get; set; }
+
+
+
+        public ArrangeCorrect(string question, List <string> trueAnswer, List <string> allAnswer)
+        {
+            Question = question;
+            AllAnswers = allAnswer;
+            TrueAnswer = trueAnswer;
+
+        }
+
+
+        public void EditQuestion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
