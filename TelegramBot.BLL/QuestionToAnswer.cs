24f1074dcc2c675c0c9d07b81dff51dcbd36cfae
Milana Maksina <m_maksinaa@mail.ru>
2022-04-15 22:09:49 +0300

    Add Class QuestionToAnswer

---
+++ TelegramBot.BLL/QuestionToAnswer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.BLL
+{
+    public class QuestionToAnswer : IQuestion
+    {
+        public string Question { get; set; }
+        public List <string> Answer { get; set; }
+        public List<string> TrueAnswer { get; set; }
+        public QuestionToAnswer(string question, List<string> answer, List<string> trueAnswer)
+        {
+            Question = question;
+            Answer = answer;
+            TrueAnswer = trueAnswer;
+        }
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
