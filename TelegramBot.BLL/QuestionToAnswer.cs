3a8ebfd0284d2214c58e7fabfeff870a714c0cd4
Milana Maksina <m_maksinaa@mail.ru>
2022-04-16 21:16:13 +0300

    Correct class QuestionEnterAnswer

--- TelegramBot.BLL/QuestionToAnswer.cs
+++ TelegramBot.BLL/QuestionToAnswer.cs
@@ -9,12 +9,12 @@ namespace TelegramBot.BLL
     public class QuestionEnterAnswer : IQuestion
     {
         public string Question { get; set; }
-        public List <string> Answers { get; set; }
-        public List<string> TrueAnswers { get; set; }
-        public QuestionEnterAnswer(string question, List<string> answer, List<string> trueAnswer)
+        public string UserAnswers { get; set; }
+        public string TrueAnswers { get; set; }
+        public QuestionEnterAnswer(string question, string answer, string trueAnswer)
         {
             Question = question;
-            Answers = answer;
+            UserAnswers = answer;
             TrueAnswers = trueAnswer;
         }
         public void Edit()
