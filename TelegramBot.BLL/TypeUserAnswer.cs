699c2cd647231673adae00219a324f3de4d501a2
Milana Maksina <m_maksinaa@mail.ru>
2022-04-17 23:32:53 +0300

    Rework TypeUserAnswer

---
+++ TelegramBot.BLL/TypeUserAnswer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.BLL
+{
+    public class TypeUserAnswer : IQuestion
+    {
+        public string Question { get; set; }
+        public string UserAnswer { get; set; }
+        public string TrueAnswer { get; set; }
+        private bool _isHasAnswered = false;
+        public TypeUserAnswer(string question, string answer, string trueAnswer)
+        {
+            Question = question;
+            UserAnswer = answer;
+            TrueAnswer = trueAnswer;
+        }
+
+        public TypeUserAnswer(string question, string answer)
+        {
+            Question = question;
+            UserAnswer = answer;
+        }
+        public void EditQuestion(string newQuestion)
+        {
+            Question = newQuestion;
+        }
+
+        public void ChangeTrueAnswer(string newTrueAnswer)
+        {
+            TrueAnswer = newTrueAnswer;
+        }
+        public void RemoveTrueAnswers()
+        {
+            TrueAnswer = "";
+        }
+
+
+    }
+}
