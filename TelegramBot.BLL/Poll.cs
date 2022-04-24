4f8920114fb7499bec8c2bb3bfdfc2c4d8aecce9
Milana Maksina <m_maksinaa@mail.ru>
2022-04-25 02:05:19 +0300

    Fixed bugs

--- TelegramBot.BLL/Poll.cs
+++ TelegramBot.BLL/Poll.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TelegramBot.BLL
+namespace TelegramBot.BL
 {
     public class Poll
     {
-        List<IQuestion> Questions { get; set; }
+        List <IQuestion> Questions { get; set; }
     }
 }
