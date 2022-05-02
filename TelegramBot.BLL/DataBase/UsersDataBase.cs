0832f02ae897dead7c90890b808dd32e87fa4e40
lena falina <aternetas@gmail.com>
2022-05-02 17:56:22 +0300

    add Save&Load in UsersDataBase

--- TelegramBot.BLL/DataBase/UsersDataBase.cs
+++ TelegramBot.BLL/DataBase/UsersDataBase.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
 
 namespace TelegramBot.BL.DataBase
 {
@@ -10,6 +12,7 @@ namespace TelegramBot.BL.DataBase
     {
         public List<Group> UserGroups { get; set; }
         private static UsersDataBase _instance;
+        private const string filePath = @"usergroups.data"; // добавить в wpf
 
         private UsersDataBase()
         {
@@ -25,6 +28,41 @@ namespace TelegramBot.BL.DataBase
                 _instance = new UsersDataBase();
             }
             return _instance;
+        }
+
+        public string Serialize(List<Group> UserGroups)
+        {
+            return JsonSerializer.Serialize<List<Group>>(UserGroups);
+        }
+
+        public List<Group> Deserialize(string json)
+        {
+            if (json == null)
+            {
+                throw new ArgumentNullException("json");
+            }
+            else
+            {
+                return JsonSerializer.Deserialize<List<Group>>(json);
+            }
+        }
+
+        public void Save(List<Group> UserGroups)
+        {
+            string json = Serialize(UserGroups);
+            using(StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.WriteLine(json);
+            }
+        }
+
+        public List<Group> Load()
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string json = sr.ReadLine();
+                return Deserialize(json);
+            }
         }
     }
 }
